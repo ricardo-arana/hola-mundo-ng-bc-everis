66205b21a5b17047092f96559ffe78e59b0b772c
Ricardo Arana Reyes <raranare@everis.com>
2020-06-04 15:45:57 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'heroes', component: HeroesComponent},
+  { path: '**', pathMatch: 'full',redirectTo: 'inicio'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SaludadorComponent } from './components/saludador/saludador.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SaludadorComponent
+    SaludadorComponent,
+    InicioComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
