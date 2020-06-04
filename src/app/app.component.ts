735e830779cf1a0393ef48bbd15c39a05bd76336
Ricardo Arana Reyes <raranare@everis.com>
2020-06-04 11:36:57 -0500

    saludador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hola-mundo';
+  title = 'hola bootcamp!';
+  numero = 6;
+  nombre = 'Ricardo';
+  apellido: string;
+  fechaNacimiento: Date;
+  fechaTexto: string;
+  saludado: string;
+
+  constructor() {
+    this.apellido = 'Arana Reyes Guerrero';
+    this.fechaNacimiento = new Date('11/02/1990');
+    this.fechaTexto = `${this.fechaNacimiento.getDate()}/${this.fechaNacimiento.getMonth()}/${this.fechaNacimiento.getFullYear()}`;
+
+  }
+
+  saludar(nombre: string) {
+    if(nombre) {
+      this.saludado = nombre;
+    }
+  }
 }
